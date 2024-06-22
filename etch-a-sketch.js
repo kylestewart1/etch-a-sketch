99975e7b0e002ce9c087f94f93972df151d917db
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-06-22 17:20:06 -0400

    added resizing and progressive darkening

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,40 @@
-const container = document.querySelector("#container");
+const screen = document.querySelector("#screen");
+const resetButton = document.querySelector("button");
 
-let gridSize = 16;
-const boxSize = 100/gridSize;
-console.log(boxSize);
+resetButton.addEventListener("click", () => {
+    const sizeInput = Number(prompt("Enter a grid size for the etch-a-sketch (max 100): "));
+    if (sizeInput > 100) {
+        alert("That's too big!");
+    } else if (sizeInput < 1) {
+        alert("Grid size must be at least 1.")
+    } else {
+        buildGrid(sizeInput);
+    }
+})
 
-for (let i = 0; i < gridSize**2; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.style.width = `${boxSize}%`;
-    box.style.height = `${boxSize}%`;
-    container.appendChild(box);
+function buildGrid(gridSize) {
+    screen.innerHTML = "";
+    const boxSize = 100/gridSize;
+
+    for (let i = 0; i < gridSize**2; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = `${boxSize}%`;
+        box.style.height = `${boxSize}%`;
+        box.style.backgroundColor = "black";
+        box.style.opacity = "0";
+        box.addEventListener("mouseover", () => {
+            const opacity = Number(box.style.opacity);
+            if (opacity < 1) {
+                box.style.opacity = `${opacity + 0.1}`;
+            }
+        })
+        screen.appendChild(box);
+    }
 }
 
-const leftKnob = document.createElement("div");
-const rightKnob = document.createElement("div");
+buildGrid(16);
+
+
+
+
