40df037cf6b7057e38e2df6d9530f3429295ce82
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-06-22 13:07:07 -0400

    Initial commit

---
+++ etch-a-sketch.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container");
+
+let gridSize = 16;
+const boxSize = 100/gridSize;
+console.log(boxSize);
+
+for (let i = 0; i < gridSize**2; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${boxSize}%`;
+    box.style.height = `${boxSize}%`;
+    container.appendChild(box);
+}
